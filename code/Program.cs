1ab50151796563f6bdd0c73063755005b2b9b74f
Valdemar Deutsch <greynervladimir@gmail.com>
2024-10-23 23:35:07 +0400

    Added method CheckString(), which checks is string's length <= 3

--- code/Program.cs
+++ code/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Задача: Написать программу, которая из имеющегося массива 
+// строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+//  При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
 Console.WriteLine("Hello, World!");
+
+string str = Console.ReadLine()!;
+string[] strArray = str.Split();
+string[] CheckString(string[] stringArray){
+    string[] ModArray = new string[stringArray.Length];
+    for (int i = 0; i != stringArray.Length; i++){
+        if (stringArray[i].Length <= 3){
+            ModArray[i] += stringArray[i];
+        }
+        else{
+            break;
+        }
+            // if (stringArray[i].Length <= 3){
+                
+            // }
+            // else{
+            //     break;
+            // }
+        }
+    }
+    return ModArray;
+}
